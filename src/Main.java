9bcc629a94ef3a48fe0a33de0d12b376f56b715d
viktor91 <v.a.taradaiko@gmail.com>
2022-06-14 06:35:40 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 2200; // стоиммость билета
+        int mile = 20; // стоимость милли
+        System.out.println("Всего бонусных миль получено:");
+        System.out.println(cost / mile + "миль");
+
+
+    }
+}
